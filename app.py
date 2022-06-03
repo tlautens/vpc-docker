df2583b4c86d3ff23a8ede8b2fea275aa25c24e6
tlautens <tim.lautenschlaeger@icloud.com>
2022-06-03 08:10:19 +0200

    Update app.py: set debug to False

--- app.py
+++ app.py
@@ -41,5 +41,5 @@ def upload_audio():
     return source_filepath
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
 
